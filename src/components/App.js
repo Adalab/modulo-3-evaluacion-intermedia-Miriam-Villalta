040411dea1fbb3e9a8cb349001d5e246741a9a9f
Miriam-Villalta <mvb_8@hotmail.com>
2021-06-08 10:55:23 +0200

    add useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../stylesheets/App.css";
 import PokeDex from "../data/PokeDex.json";
 import PokeList from "./PokeList";
 
 function App() {
+  const [pokemons] = useState(PokeDex);
+
   return (
     <>
       <h1 className="title">Mi lista de Pokemon</h1>
-      <PokeList list={PokeDex} />
+      <PokeList list={pokemons} />
     </>
   );
 }
